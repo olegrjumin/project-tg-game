42a644f9b48812d44fcb450008389baf8b2206c9
gpereira-mwb <168778782+gpereira-mwb@users.noreply.github.com>
2024-06-04 15:57:06 +0100

    game rendering once only inside game route

--- src/pages/Game/game.tsx
+++ src/pages/Game/game.tsx
@@ -1,13 +1,15 @@
 import Phaser from "phaser";
 import { FallingObjects } from "../../game/scenes/FallingObjects";
+import { useEffect, useRef } from "react";
 
 export const Game = () => {
+  const phaserGameRef = useRef<any>(null);
   const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    parent: "phaser-container",
     backgroundColor: "#282c34",
-    width: 800,
-    height: 600,
+    parent: 'game',
+    width: window.innerWidth,
+    height: window.innerHeight,
     physics: {
       default: "arcade",
       arcade: {
@@ -17,7 +19,16 @@ export const Game = () => {
     scene: FallingObjects,
   };
 
-  new Phaser.Game(config);
+  useEffect(() => {
+    if (phaserGameRef.current) {
+      return;
+    }
+    phaserGameRef.current = new Phaser.Game(config);
+    return () => {
+      phaserGameRef.current.destroy(true);
+      phaserGameRef.current = null;
+    };
+  } , [])
 
-  return null;
+  return <div id='game'></div>;
 };
