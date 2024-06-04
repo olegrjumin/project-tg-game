90ffdd31097c9a68886251596e92c15cc64f872c
Oleg Rjumin <oleg.rjumin@gmail.com>
2024-06-04 14:48:09 +0300

    update

--- src/pages/Game/game.tsx
+++ src/pages/Game/game.tsx
@@ -2,17 +2,22 @@ import Phaser from "phaser";
 import { FallingObjects } from "../../game/scenes/FallingObjects";
 
 export const Game = () => {
-
   const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     parent: "phaser-container",
     backgroundColor: "#282c34",
     width: 800,
     height: 600,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 0, x: 0 },
+      },
+    },
     scene: FallingObjects,
   };
 
-  const game = new Phaser.Game(config);
+  new Phaser.Game(config);
 
-  return <></>
+  return null;
 };
