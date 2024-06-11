32f4db45d506d4731d1ae20da85b3669afb11144
gpereira-mwb <168778782+gpereira-mwb@users.noreply.github.com>
2024-06-11 18:33:57 +0100

    fixed score multiplier, fixed font

--- src/game/useCreateFallingVirusesScene.ts
+++ src/game/useCreateFallingVirusesScene.ts
@@ -1,12 +1,12 @@
 import { useHapticFeedback } from "@tma.js/sdk-react";
-import { useGetInvitees } from "../hooks/use-get-invitees";
+// import { useGetInvitees } from "../hooks/use-get-invitees";
 import { FallingViruses } from "./scenes/FallingViruses";
 
 export const useCreateFallingVirusesScene = (
   callbackFunction: (score: number) => void,
 ) => {
   const haptics = useHapticFeedback();
-  const invitees = useGetInvitees();
+  const invitees = ['1', '2', '3', '4', '5', '6'];
   const scoreMultiplier = invitees && invitees.length ? invitees.length + 1 : 1;
   return class extends FallingViruses {
     constructor() {
